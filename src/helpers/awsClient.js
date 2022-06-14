f29fa9b40ffdbd1468d6476d829eafba4c2a05f3
astabean <malyskaite.asta@gmail.com>
2022-06-14 22:20:20 +0100

    Fixed issue where variable was not used in a correct way

--- src/helpers/awsClient.js
+++ src/helpers/awsClient.js
@@ -2,8 +2,8 @@ import AWS from 'aws-sdk'
 import { getAwsRegion, isLocalEnvironment } from './getEnvironmentVariables'
 
 const getS3Client = () => {
-  const endpoint = isLocalEnvironment() ? 'http://localhost:4569' : undefined
-  console.info(`Is local env - ${isLocalEnvironment()}`)
+  const endpoint = isLocalEnvironment === true ? 'http://localhost:4569' : undefined
+  console.info(`Endpoint - ${endpoint}`)
   return new AWS.S3({ signatureVersion: 'v4', region: getAwsRegion(), endpoint })
 }
 
